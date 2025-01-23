8752dae9d883dc29c89c3e098ad1b44465a0bb2c
Mg Thant <tzinwin517@gmail.com>
2025-01-23 18:36:53 +0630

    add_about_&_exp_section_using_framer_motion

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -24,10 +24,12 @@ const Hero = () => {
             <div className="flex flex-col justify-center items-center mt-5">
               <div className="w-5 h-5 rounded-full bg-[#915eff] z-5" />
               <div
-                class="w-1 -mt-3
+                className="w-1 -mt-3
                 border-l-[5px] border-l-transparent
-                border-t-[300px] border-t-[#915eff]
-                border-r-[1px] border-r-transparent"
+                border-t-[200px] border-t-[#915eff]
+                border-r-[1px] border-r-transparent
+                sm:border-t-[300px]
+                "
               ></div>
             </div>
             <div>
@@ -41,6 +43,21 @@ const Hero = () => {
             </div>
           </div>
           <ComputersCanvas />
+          <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+            <a href="#about">
+              <div className="w-[32px] h-[64px] rounded-3xl border-2 border-secondary flex justify-center items-start p-2">
+                <motion.dev
+                  animate={{ y: [0, 30, 0] }}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }}
+                  className="w-3 h-3 rounded-full bg-secondary mb-1"
+                />
+              </div>
+            </a>
+          </div>
         </section>
       </div>
       <About />
--- frontend/src/components/canvas/Computers.jsx
+++ frontend/src/components/canvas/Computers.jsx
@@ -4,19 +4,37 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-
+  
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
-      <primitive object={computer.scene} dispose={null} />
+      <hemisphereLight intensity={2.5} groundColor="black" />
+      <pointLight intensity={2} />
+      <spotLight intensity={1} position={[-20, 50, 10]} angle={0.12} penumbra={1} castShadow shadow-mapSize={1024} />
+      <primitive object={computer.scene} scale={isMobile ? 0.5 : 0.75} position={isMobile ? [-2.8, -2.5, -1.5] : [0, -3.5, -1.5]} rotation={[ -0.01, -0.2, -0.1]} dispose={null} />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const screenSize = window.matchMedia("(max-width: 500px)");
+    
+    setIsMobile(screenSize.matches);  
+
+    const handleScreenSizeChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    screenSize.addEventListener("change", handleScreenSizeChange);
+
+    return () => {
+      screenSize.removeEventListener("change", handleScreenSizeChange);
+    } // remove for components that don't unmount
+  }, []);
   return (
     <Canvas
       frameloop="demand"
@@ -30,7 +48,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
